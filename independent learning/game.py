23b99f36ba26827149987981eceea0498c3d62c8
Sukie9 <156082279+Sukie9@users.noreply.github.com>
2024-02-04 16:50:34 +0000

    Update game.py

--- independent learning/game.py
+++ independent learning/game.py
@@ -1,4 +1,4 @@
->>> print("---------猜数字---------")
+>>> print("---------Guess Number---------")
 import random
 a = random.randint(0, 9)
 b = random.randint(0, 9)
@@ -7,31 +7,31 @@ d = random.randint(0, 9)
 answer = a * 1000 + b * 100 + c * 10 + d
 counts = 100
 while counts > 0:
-    temp = input("猜一猜我心里的四位数字吧哈哈哈：")
+    temp = input("Guess the four digits I have in mind :")
     guess = int(temp)
     e = guess // 1000
     f = ( guess - e * 1000) // 100
     g = ( guess - e * 1000 - f * 100) // 10
     h = ( guess - e * 1000 - f * 100 - g * 10)
     if guess == answer:
-        print("你猜对啦")
-        print("不过猜对了也没有奖励,嘻嘻嘻~~~")
-        print("游戏结束啦")
+        print("You got it.")
+        print("But there's no reward for guessing correctly~")
+        print("Game over.")
         counts = counts - 1
-        print("次数:",end="");print(100 - counts)
+        print("number of times:",end="");print(100 - counts)
         break
     else:
-        print("次数：",end="");print(101- counts)
+        print("number of times：",end="");print(101- counts)
         if a == e:
-            print("第一位对喽")
+            print("The first one's right.")
         if b == f:
-            print("第二位对喽")
+            print("The second one's right")
         if c == g:
-            print("第三位对喽")
+            print("The third one's right")
         if d == h:
-            print("第四位对喽")    
+            print("The fourth one's right")    
         if guess > answer:
-            print("不过大了呢快快快继续继续")
-            print("继续猜吧")
+            print("But it's bigger. Come on, come on, come on. Keep going.")
+            print("Continue")
         else:
-            print("不过小了呢")
+            print("But it's smaller")
